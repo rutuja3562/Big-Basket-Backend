f699a5d6412eda2dd30ab102e0838eefcb3c5806
rutuja3562 <rspatil3562@gmail.com>
2022-08-23 23:25:41 +0530

    razorpayment updated

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ app.use(cors());
 const vegetableController = require("./controller/vegetable.controller")
 const vegetableCartController = require("./controller/vegetablecart.controller")
 // const payment = require("./paymentRozerpay");
-const payment=require("./Razor")
+const payment=require("./paymentRazorpay")
 
 app.use("/logo", payment);
 app.use("/razorpay", payment);
