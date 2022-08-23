810f49bb03cb15bf7d61c5d9b28cc40dd8ce9684
rutuja3562 <rspatil3562@gmail.com>
2022-08-24 02:41:07 +0530

    razorpayment updated1

--- src/paymentRazorpay.js
+++ src/paymentRazorpay.js
@@ -1,48 +1,5 @@
 const express = require("express");
 
-// require("dotenv").config();
-// const router = express.Router();
-
-// const shortid = require("shortid");
-// const Razorpay = require("razorpay");
-
-// const razorpay = new Razorpay({
-//   key_id: "rzp_test_Mg2MeEGX1Eyeh2",
-//   key_secret: "73FnKAwKjspj3HLURbijmTGi",
-// });
-
-// router.get("", (req, res) => {
-//   res.sendFile(path.join(__dirname, "paymentlogo.jpg"));
-// });
-
-// router.post("", async (req, res) => {
-//   const payment_capture = 1;
-//   //   const amount = req.query.price;
-//   const amount = 499;
-//   const currency = "INR";
-
-//   const options = {
-//     amount: amount * 100,
-//     currency,
-//     receipt: shortid.generate(),
-//     payment_capture,
-//   };
-
-//   try {
-//     const response = await razorpay.orders.create(options);
-//     console.log("LL", response);
-//     res.json({
-//       id: response.id,
-//       currency: response.currency,
-//       amount: response.amount,
-//     });
-//   } catch (error) {
-//     console.log(error);
-//   }
-// });
-
-// module.exports = router;const express = require("express");
-// const path = require("path");
 require("dotenv").config();
 const router=express.Router()
 
@@ -51,16 +8,12 @@ const shortid = require("shortid");
 const Razorpay = require("razorpay");
 
 const razorpay = new Razorpay({
-  // key_id: process.env.KEY_ID,
-  // key_secret: process.env.SECRET_KEY,
-  key_id: "rzp_test_Mg2MeEGX1Eyeh2",
-    key_secret: "73FnKAwKjspj3HLURbijmTGi",
+  key_id: process.env.KEY_ID,
+  key_secret: process.env.SECRET_KEY,
 });
 
-// Serving company logo
 
-
-router.post("", async (req, res) => {            ////////razorpay
+router.post("", async (req, res) => {            
   console.log(req.query.price)
   const payment_capture = 1;
   const amount = req.query.price;
