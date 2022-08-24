da790ce945448893b15316463ba2b38caee6685a
rutuja3562 <rspatil3562@gmail.com>
2022-08-24 13:15:38 +0530

    razorpayment added

--- src/models/pay.module.js
+++ src/models/pay.module.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const razorPaySchema = new mongoose.Schema(
   {
     paymentid: { type: String, required: true },
+    id: { type: String },
   },
   {
     versionKey: false,
