c2482d1232f4c47ac8b1d1bb10f0298696887777
unknown <rspatil3562@gmail.com>
2022-09-23 15:42:28 +0530

    update

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,7 +1,8 @@
 const mongoose=require("mongoose");
 
 const connect=()=>{return mongoose.connect(
-  "mongodb+srv://Rutu:Rutuatlas3562@cluster0.4soie.mongodb.net/bigBasket?retryWrites=true&w=majority"
+  "mongodb+srv://Rutu:RutujaAtlas@cluster0.4soie.mongodb.net/bigBasket?retryWrites=true&w=majority"
+  // "mongodb+srv://Rutu:RutujaAtlas@cluster0.4soie.mongodb.net/bigBasket?retryWrites=true&w=majority"
 );
 };
 module.exports=connect;
