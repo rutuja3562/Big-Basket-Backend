a3f096abd7773b5d3310db3bd33cc18c541f386e
Rutuja Patil <rspatil3562@gmail.com>
2022-11-24 11:37:12 +0530

    database url change

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,10 +1,8 @@
 const mongoose=require("mongoose");
 
 const connect=()=>{return mongoose.connect(
-  // process.env.URL
-  "mongodb+srv://Rutu:RutujaAtlas@cluster0.4soie.mongodb.net/bigBasket?retryWrites=true&w=majority"
-  // "mongodb+srv://Rutu:RutujaAtlas@cluster0.4soie.mongodb.net/bigBasket?retryWrites=true&w=majority"
-);
+  process.env.URL
+  );
 };
 module.exports=connect;
 
