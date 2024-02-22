e6d980553d6e15266e4e32cb9803881a24b11fa4
Rutuja Patil <rspatil3562@gmail.com>
2024-02-22 14:51:35 +0530

    added

--- src/configs/db.js
+++ src/configs/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
 const connect = () => {
-  return mongoose.connect(process.env.URL);
+  return mongoose.connect("mongodb+srv://Rutu:Rsp3562@cluster0.4soie.mongodb.net/bigBasket?retryWrites=true&w=majority");
 };
 module.exports = connect;
--- src/index.js
+++ src/index.js
@@ -12,9 +12,9 @@ app.use(cors());
 const userController = require("./controller/user.controller")
 const vegetableController = require("./controller/vegetable.controller")
 const vegetableCartController = require("./controller/vegetablecart.controller")
-const payment = require("./paymentRazorpay")
-const razorPayController = require("./controller/Pay.controller")
-const {register,login, generateToken} = require("./controller/auth.controller")
+ const payment = require("./paymentRazorpay")
+ const razorPayController = require("./controller/Pay.controller")
+ const {register,login, generateToken} = require("./controller/auth.controller")
 
 app.get("/logo", (req, res) => {
   res.sendFile(path.join(__dirname, "pay.jpg"));
@@ -22,7 +22,7 @@ app.get("/logo", (req, res) => {
 
 app.use("/users", userController)
 app.post("/register", register)
-app.post("/login", login)
+app.post("/login", login);
 app.use("/razorpay", payment);
 app.use("/rasor", razorPayController);
 app.use("/vegetables", vegetableController);
@@ -33,6 +33,7 @@ app.listen(port, async () => {
         await connect();
         console.log("listing on port 7005")
     } catch (err) {
+        console.log("EEEE")
         console.log(err.message);
     }
 
